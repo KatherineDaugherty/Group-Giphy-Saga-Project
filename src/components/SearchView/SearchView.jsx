9c97f84c34bd323a9a8e7730f8590c77f8db756f
Jacob Birkeland <birkeland02@gmail.com>
2021-11-04 10:28:14 -0500

    created SearchView component and added text and input

---
+++ src/components/SearchView/SearchView.jsx
@@ -0,0 +1,17 @@
+
+
+function SearchView() {
+
+    return (
+        <div>
+            <p>The Fantastic World of Giphys!!!</p>
+            <input 
+            type='text' 
+            value='Search Giphys!' 
+            onChange='TBD'/>
+        </div>
+    );
+}
+
+
+export default SearchView;
